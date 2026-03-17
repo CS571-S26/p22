6f893dad87b0111455d3bf401e64b01183e3ca3a
Annabelle Kim <ackim5@wisc.edu>
2026-03-17 18:03:36 -0500

    Created artwork page.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
+import ArtworkPage from './pages/Artwork.jsx'
 import Layout from './components/Layout.jsx'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="artwork" element={<ArtworkPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
