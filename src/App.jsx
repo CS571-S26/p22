6497a13fb131600e1b38a792e0be15b346cd6351
Annabelle Kim <ackim5@wisc.edu>
2026-03-17 18:08:21 -0500

    Added artwork page successfully, edited Layout.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
 import NotFoundPage from './pages/NotFoundPage.jsx'
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,6 +7,7 @@ export default function Layout() {
       <nav className="m-3">
         <Link to="/" className="me-3">Home</Link>
         <Link to="/about/">About</Link>
+        <Link to="/artwork" className="ms-3">Artwork</Link>
       </nav>
 
       <main>
