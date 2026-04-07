0423160df266fdb5e9b51ee79d631937dc159a42
Annabelle Kim <ackim5@wisc.edu>
2026-04-07 16:45:48 -0500

    Added the homepage background image and styled the buttons to have transparency.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,27 +7,19 @@ export default function HomePage() {
 
   return (
     <div className="home-container">
-      <Card className="custom-card shadow-lg">
         <Card.Body className="text-center">
-          <h1 className="mb-4 text-primary">Welcome</h1>
-
           <div className="d-flex flex-column align-items-center gap-3">
             <button
               className="menu-button"
               onClick={() => navigate('/about')}
-            >
-              About
-            </button>
+            >About</button>
 
             <button
               className="menu-button"
               onClick={() => navigate('/artwork')}
-            >
-              Art Gallery
-            </button>
+            >Art Gallery</button>
           </div>
         </Card.Body>
-      </Card>
     </div>
   )
 }
