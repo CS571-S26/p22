6d1c5404c8b9015ae0c35198b4ea6ca0373f2561
Annabelle Kim <annabellekim9701@gmail.com>
2026-04-22 17:13:43 -0500

    About page created.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,15 +1,46 @@
 import '../App.css'
 import { Card } from 'react-bootstrap'
+import bg from '../assets/Page.png'
 
 export default function AboutPage() {
   return (
-    <div className="w-100 h-100 d-flex justify-content-center align-items-center">
-      <Card className='m-4 p-2'>
-        <Card.Body className='text-center'>
-          <h2>About</h2> 
-          <p>Example declarative routing with react-router-dom.</p>
-        </Card.Body>
-      </Card>
+    <div
+      className="page-container"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="about-card">
+        <h1 className="about-title">About Me</h1>
+
+        <p className="about-text">
+          Hi! I’m Annabelle Kim, a student at UW-Madison who is passionate about game development, design, and building creative digital experiences.
+        </p>
+
+        <p className="about-text">
+          This website is dedicated towards my projects, artwork, and other accomplishments to display my skills and interests in a fun and interactive way.
+        </p>
+
+        <p className="about-text">
+          This will include game design projects, digital art, sprites, and more! I hope you enjoy exploring my portfolio and learning more about me.
+        </p>
+
+        <div className="about-section">
+          <h3>Recent Projects</h3>
+          <ul>
+            <li>Strung Along (Game Jame Submission)</li>
+            <li>Personal Website Design</li>
+            <li>Into the Pit (Game Design Project)</li>
+          </ul>
+        </div>
+
+        <div className="about-section">
+          <h3>Links</h3>
+          <ul>
+            <li ><a href="https://www.linkedin.com/in/annabelle-kim-7b9620382/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+            <li><a href="https://itch.io/profile/wucke" target="_blank" rel="noopener noreferrer">Itchio</a></li>
+            <li><a href="https://www.artstation.com/wuckeee8" target="_blank" rel="noopener noreferrer">Artstation</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
